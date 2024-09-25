56b5250572566bcddf402ab7cc39a939c23bc7be
ckelly27 <cameron.kelly04@icloud.com>
2024-09-24 18:05:15 -0700

    added to main

--- main.py
+++ main.py
@@ -2,7 +2,39 @@ from storage import storeData
 from fetch import getNBAData
 import pandas as pd
 from datetime import date
+import sys
 import time
+
+
+def getData():
+            
+    # Fetch the NBA data using the getNBAData class from fetch.py
+            dataFetcher = getNBAData(currMonth, currYear)
+            
+            # Fetch the data (which will populate the gameData)
+            gameData = dataFetcher.getGameData()
+            
+            # Store the data using the storeData class
+            data_saver = storeData(gameData)
+            data_saver.saveToCSV()
+
+def checkCSV():
+    ans = getInput("Do you already have the up-to-date NBA CSV? (y/n) ", ("y", "n"))
+    if ans == "n":
+        if getInput("This process will take about 30 min. Is this okay? (y/n) ", ("y", "n")) == "y":
+             getData()
+        else:
+             sys.exit(0)
+        
+        
+def getInput(text, responses):
+       
+    while True:
+        ans = input(text).strip().lower()
+        if ans not in responses:
+            print("Please enter a valid answer")
+        else:
+            return ans 
 
 if __name__ == "__main__":
     # Dictionary of months of the NBA season
@@ -15,26 +47,13 @@ if __name__ == "__main__":
     # If current month isn't during NBA season, defaults to june (end of last season)
     if currMonth == None:
         currMonth = "june"
-
-    # TEMPORARY
-    print(currYear)
-    print(currMonth)
-
-    # Start program timer
-    start_time = time.time()
-
-    # Fetch the NBA data using the getNBAData class from fetch.py
-    dataFetcher = getNBAData(currMonth, currYear)
     
-    # Fetch the data (which will populate the gameData)
-    gameData = dataFetcher.getGameData()
+    checkCSV()
+    df = pd.read_csv("NBAGameData.csv")
     
-    # Below retrieves the list of error codes encountered
-    #print(dataFetcher.getError())
-    
-    # Store the data using the storeData class
-    data_saver = storeData(gameData)
-    data_saver.saveToCSV()
 
-    # Print program runtime
-    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+    
+
+            
